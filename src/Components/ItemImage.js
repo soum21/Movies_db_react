bcbfc22fe1f24c55d1e9705d0df265e38bda74e3
Towfiqur Rahman <towfiqur@stubapp.com>
2019-08-27 22:24:04 +0800

    added pagination fix

--- src/Components/ItemImage.js
+++ src/Components/ItemImage.js
@@ -7,9 +7,9 @@ const ItemImage = (props) => {
 
         <div className="card-image waves-effect waves-block waves-light" style={{ textAlign: 'center' }}>
             {
-                props.image == null ? (<img src={`https://cdn11.bigcommerce.com/s-gho61/stencil/31cc7cb0-5035-0136-2287-0242ac11001b/e/3dad8ea0-5035-0136-cda0-0242ac110004/images/no-image.svg`} alt="card image" style={{ width: "100%", height: 360 }} />)
+                props.image === null ? (<img src={`https://cdn11.bigcommerce.com/s-gho61/stencil/31cc7cb0-5035-0136-2287-0242ac11001b/e/3dad8ea0-5035-0136-cda0-0242ac110004/images/no-image.svg`} alt="card image" style={{ width: "100%", height: 360 }} />)
                     : (
-                            <img src={props.image} />
+                            <img src={props.image} alt= "No image"/>
                     )
             }
 
